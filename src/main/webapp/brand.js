53b3d4c58d7681512fbbe9a1b5f53d42848f7f38
Alessandro <alecigliano1@gmail.com>
2026-02-02 19:03:26 +0100

    Update brand.js

--- src/main/webapp/brand.js
+++ src/main/webapp/brand.js
@@ -80,9 +80,8 @@ sr.reveal('.info__img', {
     delay: 300 
 });
 
-sr.reveal('.home__swiper', {origin: 'right', distance: '300px', delay: 800})
+
 sr.reveal('.home__data', {origin: 'bottom', distance: '120px', delay: 1600})
-sr.reveal('.swiper-pagination-bullet', {origin: 'top', delay: 1800, opacity:0})
 sr.reveal('.home__button', {origin: 'top', delay: 2200})
 sr.reveal('.about__data, .contact__content', {origin: 'left'})
 sr.reveal('.about__video, .contact__img', {origin: 'right'})
